0ee487893575bafa8da85fc42ed5102eb62fdd42
Spitrec <spitrec@mail.ru>
2022-02-12 11:18:28 +0300

    Удалили ненужные файлы

--- app.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end('Hello World');
-});
-
-server.listen(80, () => console.log('start'));
--- variables.js
+++
@@ -1,5 +0,0 @@
-let name = "Armen";
-
-let age = 12;
-
-console.log(name);
