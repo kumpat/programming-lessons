fb0a119b7470ac8d91d5c7f91b5a5dfb35eede7f
Spitrec <spitrec@mail.ru>
2022-04-14 12:41:36 +0300

    скачал картинки

--- kartinki.js
+++ kartinki.js
@@ -1,15 +1,21 @@
 const axios = require('axios');
 const fs = require("fs");
-axios.get('https://http.cat/404', {responseType: 'arraybuffer'})
-.then(function (response)
-{
-fs.writeFileSync("foto.jpg", response.data)
-console.log(response);
-})
-.catch(function (error)
-{
-console.log(error);
-})
-.then(function ()
+
+   axios.get('https://http.cat/404', {responseType: 'arraybuffer'})
+   .then(function (response)
+       {
+
+       fs.writeFileSync("foto.jpg", response.data)
+       console.log(response);
+       })
+
+
+
+        .catch(function (error)
+        {
+        console.log(error);
+        })
+
+        .then(function ()
 {
 });
